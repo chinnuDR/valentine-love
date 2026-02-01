517a90e23c09de594bc8b7a3836a1974e4dd6298
chinnuDR <145547241+chinnuDR@users.noreply.github.com>
2026-02-01 19:44:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+// 🎵 Select background music
+const bgMusic = document.querySelector("#bg-music");
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    // 🎵 Play romantic music when she clicks YES
+    bgMusic.play();
+
+    setTimeout(() => {
+        window.location.href = "yes_page.html";
+    }, 1000);
+}
